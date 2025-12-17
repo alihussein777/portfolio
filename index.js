442a71f65b71d9d37a9991ac3146b661b385cbfd
AliHussein <ameen.ali.aa18@gmail.com>
2025-12-17 14:20:49 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-console.log('ali hussein')
+(function () {
+    emailjs.init("PUBLIC_KEY");
+    // ضع الـ Public Key هنا
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "SERVICE_ID",   // Service ID
+        "TEMPLATE_ID",  // Template ID
+        this
+    )
+        .then(function () {
+            alert("SEE SOON");
+        }, function (error) {
+            alert("something wrong ❌");
+            console.log(error);
+        });
+});
