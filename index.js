eee8ecc284ab91e810597c9e6d0e97a4c65caf88
AliHussein <ameen.ali.aa18@gmail.com>
2026-01-28 13:26:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-
-(function () {
-    emailjs.init("PUBLIC_KEY");
-    // ضع الـ Public Key هنا
-})();
-
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    emailjs.sendForm(
-        "SERVICE_ID",   // Service ID
-        "TEMPLATE_ID",  // Template ID
-        this
-    )
-        .then(function () {
-            alert("SEE SOON");
-        }, function (error) {
-            alert("something wrong ❌");
-            console.log(error);
-        });
-});
+console.log(hallow world);
+let num = '1', '2', '3'
